4364d3bf0b222ca8f81f6c00f2f148e3aa2c7a12
Felipekod <55560328+Felipekod@users.noreply.github.com>
2021-11-09 08:01:07 -0500

    React Doc
    Exercices et études de la documentation officielle React

---
+++ exercices_1/src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props => 
+    <Main icon="home" title="Hey" subtittle="Portifolio">
+        <div className='display-4'>Bienvenue!</div>
+        <hr/>
+        <p className="mb-0">Ici je garde mes exercices et references React!</p>
+    </Main>
