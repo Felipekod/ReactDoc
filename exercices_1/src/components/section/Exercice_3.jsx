033faa570d64d078ad22fa9c570cbc19b1727193
Felipekod <55560328+Felipekod@users.noreply.github.com>
2021-11-11 08:04:53 -0500

    List and Keys

--- exercices_1/src/components/section/Exercice_3.jsx
+++ exercices_1/src/components/section/Exercice_3.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import Main from '../template/Main'
 
+const numbers = [1, 2, 3, 4, 5]
+const listItems = numbers.map(number => <li>{number}</li>)
+
 export default props => 
     <Main icon="home" title="Exercices 3" subtittle="Portifolio">
-        <div className='display-3'>Exercices 3!</div>
+        <div className='display-4'>Exercices 3!</div>
         <hr/>
+        <h2>Lists and Keys</h2>
+        <ul>{listItems}</ul>
+        
         
     </Main>
