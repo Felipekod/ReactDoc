d5c51e99528401aa7b8e44a316ddf41a268c5d3c
Felipekod <55560328+Felipekod@users.noreply.github.com>
2021-11-11 10:01:20 -0500

    backend

--- exercices_1/src/components/section/Exercice_4.jsx
+++ exercices_1/src/components/section/Exercice_4.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import Main from '../template/Main'
 
+const fs = require('fs')
+const dir = '../../assets/pictures'
+//On compte les photos dans le dossier
+let totalPhotos = 0
+function totalPhotosFun(dir){
+    fs.readdir(dir, (error, files) => {
+        return  files.length
+    })
+}
+totalPhotos = totalPhotosFun(dir)
+
+
+console.log("TOTAL:" + totalPhotos)
+
 
 export default props => 
     <Main icon="home" title="Exercices 4" subtittle="Portifolio">
         <div className='display-4'>Exercices 4!</div>
         <hr/>
         <h2>Quelques photos à moi</h2>
-        
+        <p>Total: {totalPhotos}</p>
         
     </Main>
