71998ffe6f3c0b350ee5e24ff10e2ffb6a7ce21f
Felipekod <55560328+Felipekod@users.noreply.github.com>
2021-11-11 10:08:58 -0500

    bug fix photos

--- exercices_1/src/components/section/Exercice_4.jsx
+++ exercices_1/src/components/section/Exercice_4.jsx
@@ -1,19 +1,7 @@
 import React from 'react'
 import Main from '../template/Main'
 
-const fs = require('fs')
-const dir = '../../assets/pictures'
-//On compte les photos dans le dossier
-let totalPhotos = 0
-function totalPhotosFun(dir){
-    fs.readdir(dir, (error, files) => {
-        return  files.length
-    })
-}
-totalPhotos = totalPhotosFun(dir)
 
-
-console.log("TOTAL:" + totalPhotos)
 
 
 export default props => 
@@ -21,6 +9,5 @@ export default props =>
         <div className='display-4'>Exercices 4!</div>
         <hr/>
         <h2>Quelques photos à moi</h2>
-        <p>Total: {totalPhotos}</p>
         
     </Main>
