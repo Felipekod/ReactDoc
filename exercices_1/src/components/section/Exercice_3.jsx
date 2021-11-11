920295ebb31a31960edc250f7e72fae342ffda67
Felipekod <55560328+Felipekod@users.noreply.github.com>
2021-11-11 08:42:17 -0500

    List and Keys

--- exercices_1/src/components/section/Exercice_3.jsx
+++ exercices_1/src/components/section/Exercice_3.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import Main from '../template/Main'
 
+//List and keys exercices
 const numbers = [1, 2, 3, 4, 5]
 const listItems = numbers.map(number => <li>{number}</li>)
+const todoItens = numbers.map((number, index) => <li key={index}>{"Numéro: " + number + ' Index: '+ index}</li>)
+
+function ListItem(props){
+    return <li>{props.value}</li>
+}
+
+function NumberList(props){
+    const numbers2 = props.numbers
+    const listItems2 = numbers2.map((number) => <ListItem key={number.toString()} value={number}/>)
+    return (
+        <ul>
+            {listItems2}
+        </ul>
+    )
+}
 
 export default props => 
     <Main icon="home" title="Exercices 3" subtittle="Portifolio">
@@ -10,6 +26,10 @@ export default props =>
         <hr/>
         <h2>Lists and Keys</h2>
         <ul>{listItems}</ul>
-        
+        <hr/>
+        <ul>{todoItens}</ul>
+        <hr/>
+        <h3></h3>
+        <NumberList numbers= {numbers}/>
         
     </Main>
