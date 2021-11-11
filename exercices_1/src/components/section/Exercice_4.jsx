a27c9975cf79daffd0280d8fa2ae245fc9ddebcd
Felipekod <55560328+Felipekod@users.noreply.github.com>
2021-11-11 10:22:53 -0500

    Axios
    Backend fonctionnel avec Axios au frontEnd

--- exercices_1/src/components/section/Exercice_4.jsx
+++ exercices_1/src/components/section/Exercice_4.jsx
@@ -1,6 +1,38 @@
-import React from 'react'
+import React, {Component} from 'react'
+import axios from 'axios'
 import Main from '../template/Main'
 
+const baseUrl = 'http://localhost:3001/photos'
+const initialState = {
+    photo: {id: '', name: ''},
+    list: []
+}
+
+class Photos extends Component {
+    state = {...initialState}
+
+    componentDidMount(){
+        axios(baseUrl).then(
+            resp => {
+                this.setState( {list: resp.data})
+            }
+        )
+    }
+
+    renderLi(){
+        return this.state.list.map(photo => {
+            return (
+                <li>teste</li>
+            )
+        })
+    }
+
+    render(){
+        return <ul>
+            {this.renderLi()}
+        </ul>
+    }
+}
 
 
 
@@ -9,5 +41,6 @@ export default props =>
         <div className='display-4'>Exercices 4!</div>
         <hr/>
         <h2>Quelques photos à moi</h2>
+        <Photos/>
         
     </Main>
