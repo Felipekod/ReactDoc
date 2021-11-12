3bb6637a19f70cd65d369dffc4cfe109f1d757fb
Felipekod <55560328+Felipekod@users.noreply.github.com>
2021-11-12 11:56:07 -0500

    react-photo-galley

--- exercices_1/src/components/section/Exercice_5.jsx
+++ exercices_1/src/components/section/Exercice_5.jsx
@@ -1,13 +1,36 @@
 import React from 'react'
 import Main from '../template/Main'
-import ImageGallery from 'react-image-gallery'
+import ImageGallery from 'react-photo-gallery'
 
+const photos = [
+    {
+        src: "https://i.imgur.com/KBrrTUW.jpg",
+        width: 4,
+        height: 3
+    },
+    {
+        src: "https://i.imgur.com/b5tIvta.jpg",
+        width: 1,
+        height: 1
+    }
+]
+
+const BasicRows = () => <ImageGallery photos={photos} />
+
+class MyGallery extends React.Component {
+    render(){
+        return (
+            <ImageGallery photos={photos}/>
+        )
+    }
+}
 
 
 export default props => 
     <Main icon="home" title="Exercices 5" subtittle="Portifolio">
         <div className='display-4'>Exercices 5!</div>
         <hr/>
+        <BasicRows className='gallery'/>
         
         
     </Main>
