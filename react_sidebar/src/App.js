993bf925f0c555506316d3e669475de41a5a77ae
Felipekod <55560328+Felipekod@users.noreply.github.com>
2021-12-02 11:05:51 -0500

    Sidebar modern
    Dernier commit avant le changement d'interface du projet principal

--- react_sidebar/src/App.js
+++ react_sidebar/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Reports from './pages/Reports'
+import Products from './pages/Products'
 
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route exact path='/' element={<Home/>}/>
+          <Route exact path='/reports' element={<Reports/>}/>
+          <Route exact path='/products' element={<Products/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
